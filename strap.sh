49d98759b16aef3e0dda8301da4da703b9283e39
Kevin Spencer <145229015+kvnspncr@users.noreply.github.com>
2024-02-12 12:31:44 +0000

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt 
-return = $?
 if [ $? -eq 0 ]; then 
   printf("Setup is done, you can run the script now using python3 main.py")
 else 
-  printf("Setup failed with return code $return")
+  printf("Setup failed")
